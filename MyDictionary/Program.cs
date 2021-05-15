fd3201771ac8077f7eafd6f74a1898c9c61b0748
Baayri <m.baayri@gmail.com>
2021-05-15 22:47:41 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "elma");
+            myDictionary.Add(1, "armut");
+            myDictionary.Add(1, "karpuz");
+        }
+    }
+}
